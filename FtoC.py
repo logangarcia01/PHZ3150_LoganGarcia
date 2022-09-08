006d52fa5241080342d8e6d443a964c2139e96ce
Logan Garcia <logan.garcia2000@knights.ucf.edu>
2022-09-08 13:17:46 -0400

    class 9/8

---
+++ FtoC.py
@@ -0,0 +1,10 @@
+def FtoC():
+    """Function that converts tempertures from F to C.
+    Input: temperature in F
+    Output: temperature in C
+    """
+    
+    c = (f - 32.)(5.0/9.0)
+    
+    
+    return C 
