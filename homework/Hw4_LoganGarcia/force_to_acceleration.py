0a30c885fe8dea783676ab2bcb6f41e289de7e2d
Logan Garcia <logan.garcia2000@knights.ucf.edu>
2022-09-15 00:43:12 -0400

    hw

---
+++ homework/Hw4_LoganGarcia/force_to_acceleration.py
@@ -0,0 +1,11 @@
+def force_to_acceleration ( F, m ): 
+    """function that finds the acceleration of am object using the force divided by the mass
+        Follows : a = F / m
+        Input : Force [N], mass [kg]
+        output : acceleration [m/s^2]"""
+    
+    a = F / m
+    
+    print( "The acceleration of the object is" , a , "m/s^2". )
+    
+    return a
